954ed6732da32fa3eeaac00c33611c455adaeb92
kimyounhoex1 <dordyd11@gmail.com>
2026-01-12 22:23:37 +0900

    init project

---
+++ log_collector/server/ClientSession.cpp
@@ -0,0 +1,28 @@
+#include "ClientSession.hpp"
+#include <unistd.h>
+#include <iostream>
+
+ClientSession::ClientSession(int fd, EpollLoop& loop) : fd(fd), loop(loop) {}
+
+void ClientSession::start() {
+    loop.addFd(fd, [this](int fd) { onData(); });
+}
+
+void ClientSession::onData() {
+    char buf[1024];
+    ssize_t n = read(fd, buf, sizeof(buf));
+    if(n <= 0) {
+        close(fd);
+        return;
+    }
+
+    buffer.append(buf, n);
+
+    size_t pos;
+    while((pos = buffer.find('\n')) != std::string::npos) {
+        std::string line = buffer.substr(0, pos);
+        buffer.erase(0, pos+1);
+        std::cout << "[LOG] " << line << std::endl;
+    }
+}
+
---
+++ log_collector/server/TcpServer.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "EpollLoop.hpp"
+#include <unordered_map>
+#include <memory>
+
+class TcpServer {
+public:
+    TcpServer(int port);
+    void run();
+private:
+    int listen_fd;
+    EpollLoop loop;
+    void acceptConnection();
+}
